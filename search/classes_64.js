81927fa6467da1d4cf2aef631f361f4131e118d4
Peng Wu <pengwu@us.ibm.com>
2013-11-07 17:09:38 -0500

    import of doxyggen docs 0.3

--- search/classes_64.js
+++ search/classes_64.js
@@ -1,11 +1,6 @@
 var searchData=
 [
-  ['dadaptor',['DAdaptor',['../classibmppl_1_1DAdaptor.html',1,'ibmppl']]],
-  ['dadaptor_3c_20directed_2c_20neo4j_5ftraits_20_3e',['DAdaptor&lt; DIRECTED, neo4j_traits &gt;',['../classibmppl_1_1DAdaptor.html',1,'ibmppl']]],
   ['dadaptor_3c_20directed_2c_20traits_20_3e',['DAdaptor&lt; DIRECTED, Traits &gt;',['../classibmppl_1_1DAdaptor_3_01DIRECTED_00_01Traits_01_4.html',1,'ibmppl']]],
-  ['dadaptor_3c_20directness_2c_20adj_5flist_5findisk_5fstorage_5ftraits_3c_20vproperty_2c_20eproperty_2c_20directness_20_3e_20_3e',['DAdaptor&lt; Directness, adj_list_indisk_storage_traits&lt; VProperty, EProperty, Directness &gt; &gt;',['../classibmppl_1_1DAdaptor.html',1,'ibmppl']]],
-  ['dadaptor_3c_20directness_2c_20adj_5flist_5flist_5fstorage_5fwith_5findex_5ftraits_3c_20vproperty_2c_20eproperty_2c_20directness_20_3e_20_3e',['DAdaptor&lt; Directness, adj_list_list_storage_with_index_traits&lt; VProperty, EProperty, Directness &gt; &gt;',['../classibmppl_1_1DAdaptor.html',1,'ibmppl']]],
-  ['dadaptor_3c_20pred_5fdirected_2c_20adj_5flist_5findisk_5fstorage_5ftraits_3c_20vproperty_2c_20eproperty_2c_20pred_5fdirected_20_3e_20_3e',['DAdaptor&lt; PRED_DIRECTED, adj_list_indisk_storage_traits&lt; VProperty, EProperty, PRED_DIRECTED &gt; &gt;',['../classibmppl_1_1DAdaptor.html',1,'ibmppl']]],
   ['dadaptor_3c_20pred_5fdirected_2c_20traits_20_3e',['DAdaptor&lt; PRED_DIRECTED, Traits &gt;',['../classibmppl_1_1DAdaptor_3_01PRED__DIRECTED_00_01Traits_01_4.html',1,'ibmppl']]],
   ['dadaptor_3c_20undirected_2c_20traits_20_3e',['DAdaptor&lt; UNDIRECTED, Traits &gt;',['../classibmppl_1_1DAdaptor_3_01UNDIRECTED_00_01Traits_01_4.html',1,'ibmppl']]],
   ['directed',['DIRECTED',['../structibmppl_1_1DIRECTED.html',1,'ibmppl']]]
--- search/classes_74.js
+++ search/classes_74.js
@@ -5,9 +5,7 @@ var searchData=
   ['task_5fallocator',['task_allocator',['../classibmppl_1_1task__allocator.html',1,'ibmppl']]],
   ['task_5fgraph_5ftraits',['task_graph_traits',['../classibmppl_1_1task__graph__traits.html',1,'ibmppl']]],
   ['task_5fmanager',['task_manager',['../classibmppl_1_1task__manager.html',1,'ibmppl']]],
-  ['task_5fmanager_3c_20ibmppl_3a_3atask_20_3e',['task_manager&lt; ibmppl::task &gt;',['../classibmppl_1_1task__manager.html',1,'ibmppl']]],
   ['task_5fmanager_3c_20task_20_3e',['task_manager&lt; task &gt;',['../classibmppl_1_1task__manager.html',1,'ibmppl']]],
   ['task_5fqueue',['task_queue',['../classibmppl_1_1task__queue.html',1,'ibmppl']]],
-  ['threadsafe_5fgraph_5fadd_5fedge_5fadaptor',['threadsafe_graph_add_edge_adaptor',['../classibmppl_1_1threadsafe__graph__add__edge__adaptor.html',1,'ibmppl']]],
   ['threadsafe_5fgraph_5fadd_5fedge_5fadaptor_3c_20g_2c_20undirected_2c_20profile_20_3e',['threadsafe_graph_add_edge_adaptor&lt; G, UNDIRECTED, profile &gt;',['../classibmppl_1_1threadsafe__graph__add__edge__adaptor_3_01G_00_01UNDIRECTED_00_01profile_01_4.html',1,'ibmppl']]]
 ];
